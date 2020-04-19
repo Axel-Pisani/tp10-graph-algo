f4984d77f22c53a6b8aeb1415d63abe0a9063629
Yann <yann.forner@etu.univ-amu.fr>
2020-04-19 15:35:11 +0200

    extract min fini

--- tp10-graph_algo/src/Dijkstra.java
+++ tp10-graph_algo/src/Dijkstra.java
@@ -15,7 +15,7 @@ public class Dijkstra {
 	public ArrayList<Integer> makeDijkstra(Integer s){
 		d.set(s,0);
 		while(simpleQueue.size() != 0 ){
-
+			Integer u = exctractMin();
 		
 		}
 
@@ -25,10 +25,18 @@ public class Dijkstra {
 
 	public Integer exctractMin(){
 		Integer min = this.simpleQueue.get(0);
-		for (ArcPond a: this.graph.getGraphPond()
+		for (Integer i: this.simpleQueue
 			 ) {
-
+				if(this.d.get(i) < this.d.get(min)){
+					min = i;
+				}
 		}
+		this.simpleQueue.set(this.simpleQueue.indexOf(min),this.simpleQueue.get(this.simpleQueue.size()-1));
+		this.simpleQueue.remove(this.simpleQueue.size()-1);
+
+		return min;
 	}
-	
+
+
+
 }//class
