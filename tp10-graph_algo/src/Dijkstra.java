a62023492b078fa0a680d9c30a4308343f65b236
axl-pis <axelpisanipro@gmail.com>
2020-04-19 17:39:12 +0200

    on continue

--- tp10-graph_algo/src/Dijkstra.java
+++ tp10-graph_algo/src/Dijkstra.java
@@ -5,8 +5,9 @@ public class Dijkstra {
 	private GraphPond graph;
 	private ArrayList<Integer> simpleQueue;
 	private ArrayList<Integer> d;
-	public Dijkstra(GraphPond graph) {
 
+
+	public Dijkstra(GraphPond graph) {
 		this.graph = graph;
 		this.simpleQueue = graph.getPeeksPond();
 		d = new ArrayList<>();
@@ -19,28 +20,22 @@ public class Dijkstra {
 			this.d.add(i,10000000);
 		}
 		this.d.set(s,0);
-		while(simpleQueue.size() != 0 ){
+		while(simpleQueue.size() != 0){
 			Integer u = exctractMin();
-			for (Integer v : this.graph.getVoisins(u)
-				 ) {
-				if( this.d.get(v) > this.d.get(u) + this.graph.getWeightOfAnArc(u,v)  ){
-					this.d.set(v,this.d.get(u)+ this.graph.getWeightOfAnArc(u,v)) ;
-				}
+			for (Integer v : this.graph.getVoisins(u)) {
+				if (this.d.get(v) > (this.d.get(u) + this.graph.getWeightOfAnArc(u,v)))
+					this.d.set(v, (this.d.get(u) + this.graph.getWeightOfAnArc(u,v)));
 			}
-		
 		}
-
 		System.out.println(this.d);
 		return this.d;
 	}
 
 	public Integer exctractMin(){
 		Integer min = this.simpleQueue.get(0);
-		for (Integer i: this.simpleQueue
-			 ) {
-				if(this.d.get(i) < this.d.get(min)){
+		for (Integer i: this.simpleQueue) {
+				if(this.d.get(i) < this.d.get(min))
 					min = i;
-				}
 		}
 		this.simpleQueue.set(this.simpleQueue.indexOf(min),this.simpleQueue.get(this.simpleQueue.size()-1));
 		this.simpleQueue.remove(this.simpleQueue.size()-1);
--- tp10-graph_algo/src/Main.java
+++ tp10-graph_algo/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		GraphPond graphPond = new GraphPond(3, 2);
+		GraphPond graphPond = new GraphPond(6, 10);
 		graphPond.toStrPond();
 		Dijkstra dijkstra = new Dijkstra(graphPond);
 	}//main
